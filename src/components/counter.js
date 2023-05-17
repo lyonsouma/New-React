fc41862a0cda2443f16ad12fea43fdfc04238063
Lyons Ouma <oumalyons26@gmail.com>
2023-05-17 21:25:34 +0300

    added input value to test conditionaly run effect

--- src/components/counter.js
+++ src/components/counter.js
@@ -2,12 +2,15 @@ import React, {useState, useEffect} from 'react'
 const Counter = () =>{
 
     const [count, setCount] = useState(0)
+    const [name, setName] = useState("")
 
     useEffect(()=>{
+        console.log(`useEffect - updating title`)
         document.title =`Clicked ${count} times`
-    })
+    },[count])
     return(
         <div>
+            <input type='text' value={name} onChange={(e)=>setName(e.target.value)}/>
             <button value={count} onClick={()=>setCount(count+1)}>Clicked {count} times</button>
         </div>
     )
