9f90e82309a311673a69cc480a51de888b397e48
Lyons Ouma <oumalyons26@gmail.com>
2023-05-17 21:04:07 +0300

    useEffect to the title

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,8 +1,11 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 const Counter = () =>{
 
     const [count, setCount] = useState(0)
 
+    useEffect(()=>{
+        document.title =`Clicked ${count} times`
+    })
     return(
         <div>
             <button value={count} onClick={()=>setCount(count+1)}>Clicked {count} times</button>
