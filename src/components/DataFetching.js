d46795eebe69f8435635efde0872dee1435ffd38
Lyons Ouma <oumalyons26@gmail.com>
2023-05-19 00:07:58 +0300

    id as a dependency array

--- src/components/DataFetching.js
+++ src/components/DataFetching.js
@@ -2,25 +2,27 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 
 function DataFetching (){
-    const [posts , setPost] =  useState([])
-    const[]
+    const [post , setPost] =  useState({})
+    const[id ,setId] = useState(1)
 
     useEffect(()=>{
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios
+        .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
         .then(res =>{console.log(res)
         setPost(res.data)
         })
         .catch(err => console.log(err))
-    },[])
+    },[id])
 
     return(
         <div>
-            <input type='text' value={} onChange={}/>
-            <ul>
+            <input type='text' value={id} onChange={e =>setId(e.target.value)}/>
+            <div>{post.title}</div>
+            {/* <ul>
                 {
                     posts.map(post =><li key={post.id}>{post.title}</li>)
                 }
-            </ul>
+            </ul> */}
         </div>
     )
     
