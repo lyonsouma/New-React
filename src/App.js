8de1a54b2a7429cb918849a77c544b71a43924e9
Lyons Ouma <oumalyons26@gmail.com>
2023-05-18 23:46:46 +0300

    rendering data to the browser

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 // import logo from './logo.svg';
 import './App.css';
-import CounterThreeB from './components/CounterThreeB';
+import DataFetching from './components/DataFetching';
+// import CounterThreeB from './components/CounterThreeB';
 // import Counter from './components/counter';
 
 function App() {
   return (
     <div className="App">
       {/* <Counter/> */}
-      <CounterThreeB />
+      {/* <CounterThreeB /> */}
+      <DataFetching />
     </div>
   );
 }
--- src/components/DataFetching.js
+++ src/components/DataFetching.js
@@ -2,11 +2,13 @@ import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 
 function DataFetching (){
-    const [post , setPost] =  useState([])
+    const [posts , setPost] =  useState([])
 
     useEffect(()=>{
         axios.get('https://jsonplaceholder.typicode.com/posts')
-        .then(res =>{console.log(res)})
+        .then(res =>{console.log(res)
+        setPost(res.data)
+        })
         .catch(err => console.log(err))
     })
 
